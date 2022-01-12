3b3c3ae7f55673133ecd353db6d99103005b09e3
Vinícius Martins <vinicius270502@gmail.com>
2022-01-12 10:04:10 -0300

    Resolução de Exercicios Python
    Resolução do exercicios 17 da lista de repetições python presente no site https://wiki.python.org.br/EstruturaDeRepeticao

--- Python/DevMartins/Estrutura de Repetição/17Factorial.py
+++ Python/DevMartins/Estrutura de Repetição/17Factorial.py
@@ -1,10 +1,18 @@
-n = int(input('Insira um valor para ser calculado o fatorial: '))
-result = 0
-count = n
+# Entrada de dados pelo usuario e declaração de variaveis
+n = int(input("Fatorial de: "))
+result = 1
+x = 1
 
-while count == 0:
-    x = n
-    result = result + (n*x)
-    x -= 1
+# Estrutura de repetição para calculo de valor fatoria desejado pelo usuario
+while x <= n:
+    result = result * x
+    x += 1
 
-print(result)
+# Eibição de resultados
+print(f"O fatorial de {n} é {result}")
+
+
+### DICIONARIO DE VARIAVEIS
+# n = numero de entrada pelo usuario para o calculo do valor fatorial
+# result = resultado final do valor fatorial de acordo com a variavel n inserida pelo usuario
+# x = contador para repetição e calculo de fatorial
